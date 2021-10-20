fc88767ac5e6c940ff39aa4f62fa0b6b84cd099b
Lou-Janssen <hj.janssen@student.han.nl>
2021-10-20 15:10:08 +0200

    Update with flooding

--- Beans/slider.py
+++ Beans/slider.py
@@ -5,7 +5,7 @@ def nothing(x):
     pass
 
 # Load image
-image = cv2.imread("C:/Users/Lou/OneDrive - HAN/EVML Cook3r 2021-2022/Lou, Tim, Jurg/Dataset/Black_pans_masked_warped/Beans2/IMG_5849.jpeg")
+image = cv2.imread("C:/Users/Lou/OneDrive - HAN/EVML Cook3r 2021-2022/Lou, Tim, Jurg/Dataset/Black_pans_masked_warped/Beans/IMG_5849.jpeg")
 testR = image.copy()
 height, width, channels = image.shape
 
@@ -25,24 +25,12 @@ cv2.createTrackbar('VMin', 'sliders', 0, 255, nothing)
 cv2.createTrackbar('HMax', 'sliders', 0, 179, nothing)
 cv2.createTrackbar('SMax', 'sliders', 0, 255, nothing)
 cv2.createTrackbar('VMax', 'sliders', 0, 255, nothing)
-cv2.createTrackbar('RMin', 'sliders', 0, 255, nothing)
-cv2.createTrackbar('GMin', 'sliders', 0, 255, nothing)
-cv2.createTrackbar('BMin', 'sliders', 0, 255, nothing)
-cv2.createTrackbar('RMax', 'sliders', 0, 255, nothing)
-cv2.createTrackbar('GMax', 'sliders', 0, 255, nothing)
-cv2.createTrackbar('BMax', 'sliders', 0, 255, nothing)
 
 # Set default value for Max HSV trackbars
 cv2.setTrackbarPos('HMax', 'sliders', 179)
 cv2.setTrackbarPos('SMax', 'sliders', 255)
 cv2.setTrackbarPos('VMax', 'sliders', 255)
 
-cv2.setTrackbarPos('RMin', 'sliders', 0)
-cv2.setTrackbarPos('GMin', 'sliders', 0)
-cv2.setTrackbarPos('BMin', 'sliders', 0)
-cv2.setTrackbarPos('RMax', 'sliders', 255)
-cv2.setTrackbarPos('GMax', 'sliders', 255)
-cv2.setTrackbarPos('BMax', 'sliders', 255)
 
 # Initialize HSV min/max values
 Canny1Val = Canny2Val = pCanny1Val = pCanny2Val = 1
@@ -62,41 +50,31 @@ while(1):
     hMax = cv2.getTrackbarPos('HMax', 'sliders')
     sMax = cv2.getTrackbarPos('SMax', 'sliders')
     vMax = cv2.getTrackbarPos('VMax', 'sliders')
-    rMin = cv2.getTrackbarPos('RMin', 'sliders')
-    gMin = cv2.getTrackbarPos('GMin', 'sliders')
-    bMin = cv2.getTrackbarPos('BMin', 'sliders')
-    rMax = cv2.getTrackbarPos('RMax', 'sliders')
-    gMax = cv2.getTrackbarPos('GMax', 'sliders')
-    bMax = cv2.getTrackbarPos('BMax', 'sliders')
     # Set minimum and maximum HSV values to display
     lower = np.array([hMin, sMin, vMin])
     upper = np.array([hMax, sMax, vMax])
     rlower = np.array([rMin, gMin, bMin])
     rupper = np.array([rMax, gMax, bMax])
     # Convert to HSV format and color threshold
-    convert = cv2.inRange(image, rlower, rupper)
-    testR = cv2.bitwise_and(image, image, mask=convert)
-    
-    cv2.imshow('test', testR)
-    hsv = cv2.cvtColor(testR, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     if(blurVal % 2 ==0):
         blurVal = (blurVal + 1)
         cv2.setTrackbarPos('Blur', 'sliders', blurVal)
-    #blur = cv2.GaussianBlur(hsv,(blurVal, blurVal),cv2.BORDER_DEFAULT)
-    mask = cv2.inRange(hsv, lower, upper)
-    result = cv2.bitwise_and(testR, testR, mask=mask)
+    blur = cv2.GaussianBlur(hsv,(blurVal, blurVal),cv2.BORDER_DEFAULT)
+    mask = cv2.inRange(blur, lower, upper)
+    result = cv2.bitwise_and(image, image, mask=mask)
     edges = cv2.Canny(result, Canny1Val, Canny2Val)
-    # im_floodfill = mask.copy()
-    # h, w = mask.shape[:2]
-    # test = np.zeros((h+2, w+2), np.uint8)
-    # # Floodfill from point (0, 0)
-    # cv2.floodFill(im_floodfill, test, (0,0), 255)
-    # im_floodfill_inv = cv2.bitwise_not(im_floodfill)
-    # im_out = mask | im_floodfill_inv
+    im_floodfill = result.copy()
+    h, w = result.shape[:2]
+    test = np.zeros((h+2, w+2), np.uint8)
+    # Floodfill from point (0, 0)
+    cv2.floodFill(im_floodfill, test, (0,0), 255)
+    im_floodfill_inv = cv2.bitwise_not(im_floodfill)
+    im_out = result | im_floodfill_inv
 
     # Display result image
     cv2.imshow('image', result)
-    cv2.imshow('flood', edges)
+    cv2.imshow('flood', im_out)
     #cv2.imshow("Edges", edges)
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
