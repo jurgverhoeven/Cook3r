d2a6766441f7a2b8ca5d604d8e816eb4d3c3698d
Lou-Janssen <47315337+Lou-Janssen@users.noreply.github.com>
2021-12-08 23:32:16 +0100

    Update Features

--- Bubble_detection/main_program.py
+++ Bubble_detection/main_program.py
@@ -1,23 +1,27 @@
 # TensorFlow and tf.keras
 import tensorflow as tf
-
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
 # Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 print(tf.__version__)
 
-waterPath = 'C:/Users/Lou/OneDrive - HAN/EVML Cook3r 2021-2022/Lou, Tim, Jurg/Dataset/Machinelearning/Bubbles/rotated'
+waterPath = 'C:/Users/Lou-J/OneDrive - HAN/EVML Cook3r 2021-2022/Lou, Tim, Jurg/Dataset/Black_pans_v2_masked_warped'
 batchSize = 32
+labelSize = 9
 # Fetch the data
 training = tf.keras.utils.image_dataset_from_directory(
-    waterPath, validation_split=0.2,
+    waterPath, validation_split=0.3,
     subset="training",
     seed=123
 )
 
 testing = tf.keras.utils.image_dataset_from_directory(
-    waterPath, validation_split=0.2,
+    waterPath, validation_split=0.3,
     subset="validation",
     seed=123
 )
@@ -26,22 +30,53 @@ class_names = training.class_names
 
 model = tf.keras.Sequential([
   tf.keras.layers.Rescaling(1./255),
-  tf.keras.layers.Conv2D(filters=10,kernel_size=(3,3), activation='relu'),
-  tf.keras.layers.MaxPooling2D(),
-  tf.keras.layers.Conv2D(filters=10,kernel_size=(3,3), activation='relu'),
+  tf.keras.layers.Conv2D(32, 3,activation='relu'),
   tf.keras.layers.MaxPooling2D(),
   tf.keras.layers.Conv2D(filters=10,kernel_size=(3,3), activation='relu'),
   tf.keras.layers.MaxPooling2D(),
   tf.keras.layers.Flatten(),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dense(2)
+  tf.keras.layers.Dense(300, activation='relu'),
+  tf.keras.layers.Dense(100, activation='relu'),
+  tf.keras.layers.Dense(labelSize)
 ])
 
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-history = model.fit(training, validation_data=testing, epochs=10)
+history = model.fit(training, validation_data=testing, epochs=5)
+
+model.summary()
+
+for layer in model.layers:
+	# check for convolutional layer
+	if 'conv' not in layer.name:
+		continue
+	# get filter weights
+	filters, biases = layer.get_weights()
+	print(layer.name, filters.shape)
+
+# Example of visualizing some filters
+# retrieve weights from the second hidden layer
+filters, biases = model.layers[1].get_weights()
+# normalize filter values to 0-1 so we can visualize them
+f_min, f_max = filters.min(), filters.max()
+filters = (filters - f_min) / (f_max - f_min)
+# plot first few filters
+plt.figure()
+n_filters, ix = 6, 1
+for i in range(n_filters):
+	# get the filter
+	f = filters[:, :, :, i]
+	# plot each channel separately
+	for j in range(3):
+		# specify subplot and turn of axis
+		ax = plt.subplot(n_filters, 3, ix)
+		ax.set_xticks([])
+		ax.set_yticks([])
+		# plot filter channel in grayscale
+		ax.imshow(f[:, :, j], cmap='gray')
+		ix += 1
 
 # Show history
 print(history.history.keys())
@@ -87,9 +122,9 @@ def plot_image(predictions_array, true_label, img):
 
 def plot_value_array(predictions_array, true_label):
   plt.grid(False)
-  plt.xticks(range(2))
+  plt.xticks(range(9))
   plt.yticks([])
-  thisplot = plt.bar(range(2), predictions_array, color="#777777")
+  thisplot = plt.bar(range(9), predictions_array, color="#777777")
   plt.ylim([0, 1])
   predicted_label = np.argmax(predictions_array)
 
@@ -101,6 +136,7 @@ def plot_value_array(predictions_array, true_label):
 num_rows = 8
 num_cols = 4
 num_images = num_rows*num_cols
+# get feature map for first hidden layer
 for test_images, test_labels in testing:
         plt.figure(figsize=(2*2*num_cols, 2*num_rows))
         predict = probability_model.predict(test_images)
@@ -111,6 +147,52 @@ for test_images, test_labels in testing:
             plot_value_array(predict[i], test_labels[i])
         plt.tight_layout()
         plt.show()
+
+        # Define a new Model, Input= image 
+        # Output= intermediate representations for all layers in the  
+        # previous model after the first.
+        successive_outputs = [layer.output for layer in model.layers[1:]]
+        #visualization_model = Model(img_input, successive_outputs)
+        visualization_model = tf.keras.models.Model(inputs = model.input, outputs = successive_outputs)
+        #Load the input image
+        for i in range(3):
+                img = test_images[i].numpy()/255.0
+                x   = img_to_array(img)                           
+                x   = x.reshape((1,) + x.shape)
+                # Rescale by 1/255
+                # Let's run input image through our vislauization network
+                # to obtain all intermediate representations for the image.
+                successive_feature_maps = visualization_model.predict(x)
+                # Retrieve are the names of the layers, so can have them as part of our plot
+                layer_names = [layer.name for layer in model.layers]
+                for layer_name, feature_map in zip(layer_names, successive_feature_maps):
+                  print(feature_map.shape)
+                  if len(feature_map.shape) == 4:
+                    
+                    # Plot Feature maps for the conv / maxpool layers, not the fully-connected layers
+                   
+                    n_features = feature_map.shape[-1]  # number of features in the feature map
+                    size       = feature_map.shape[ 1]  # feature map shape (1, size, size, n_features)
+                    
+                    # We will tile our images in this matrix
+                    display_grid = np.zeros((size, size * n_features))
+                    
+                    # Postprocess the feature to be visually palatable
+                    for i in range(n_features):
+                      x  = feature_map[0, :, :, i]
+                      x -= x.mean()
+                      x /= x.std ()
+                      x *=  64
+                      x += 128
+                      x  = np.clip(x, 0, 255).astype('uint8')
+                      # Tile each filter into a horizontal grid
+                      display_grid[:, i * size : (i + 1) * size] = x
+                # Display the grid
+                    scale = 20. / n_features
+                    plt.figure( figsize=(scale * n_features, scale) )
+                    plt.title ( layer_name )
+                    plt.grid  ( False )
+                    plt.imshow( display_grid, aspect='auto', cmap='viridis' )
         break
 
 from sklearn.metrics import confusion_matrix
